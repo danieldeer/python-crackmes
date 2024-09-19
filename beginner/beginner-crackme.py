896ae77348708cde53ccfd8ff0b8f1197aef7ee0
Daniel Hirsch <daniel.gitarre2@gmail.com>
2024-09-19 08:05:07 -0400

    Rename folders and files

---
+++ beginner/beginner-crackme.py
@@ -0,0 +1,4 @@
+def h(i): return "".join(list(i[::-1]));
+c = h("ZYXWVUTSRQPONMLKJIHGFEDCBA"[::-1])[::-1];d = h(h('CSYJSYRHERSXLIVJPEK337'));e = 4
+def f(a, b): return "".join([z if z.upper()not in c else c[(c.index(z.upper())+b) % len(c)]for z in a]);
+g = input("Enter correct key: ");print("Yes! You found the right flag!") if g.__eq__(f(d, len(c)-e)) else print("Well, try again!")
